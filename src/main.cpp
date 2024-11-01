4f4bb6f0018d756509856e2099dd1dc7c6786b06
famsgabriel <gabriel.famss@gmail.com>
2024-11-01 14:59:52 -0300

    Exercicio 3 Resolvido - Correção Empate

--- src/main.cpp
+++ src/main.cpp
@@ -72,6 +72,9 @@ bool make_move(char player, int row, int col) {
 }
 
 bool check_draw() {
+    if(check_win()){
+	return false;
+    }
     for (int i = 0; i < 3; ++i) {
         for (int j = 0; j < 3; ++j) {
             if (board[i][j] == ' ') {
